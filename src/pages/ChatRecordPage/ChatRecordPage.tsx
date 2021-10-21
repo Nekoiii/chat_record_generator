6399e8547f6151cb2c3ea21dd40c6def6c9853b7
Nekoiii <10nekosa@gmail.com>
2021-10-21 22:34:27 +0900

    changed some lines.and fixed some style bugs

--- src/pages/ChatRecordPage/ChatRecordPage.tsx
+++ src/pages/ChatRecordPage/ChatRecordPage.tsx
@@ -19,7 +19,7 @@ const ChatRecordPage = (props: any) => {
   char: 0:♀-猫草, 1:♀-被盗当事人, 2:♂-爱凑热闹,说话比较幼稚, 3:♂-程序员,理性派,
    4:♂-东北大哥,曾被骗过,容易冲动, 5:偶尔出现的群友, 6:偶尔出现的群友
   */
-  const split_words_1 = words_obj.words_1.split('$');
+  const split_words_1 = words_obj.words_1.words.split('$');
   const chatRecords_1: chatRecords[] = [];
   split_words_1.map(it => {
     const [char, words] = it.split('#');
@@ -35,15 +35,56 @@ const ChatRecordPage = (props: any) => {
   char: 0:♀-被盗当事人, 1:♀-土豪朋友
   */
 
+  const url = '../../resources/icons/a.png';
+
+  /*
+  获取头像
+  id：角色id, me_id: 现在在哪个角色的视角
+   */
+  const get_icon_src = (id: number | string) => {
+    let icon_src: string = '';
+    switch (id) {
+      case 0:
+        icon_src = require('../../resources/icons/a.png').default;
+        break;
+      case 1:
+        icon_src = require('../../resources/icons/b.png').default;
+        break;
+      case 2:
+        icon_src = require('../../resources/icons/c.png').default;
+        break;
+      case 3:
+        icon_src = require('../../resources/icons/d.png').default;
+        break;
+      case 4:
+        icon_src = require('../../resources/icons/e.png').default;
+        break;
+      case 5:
+        icon_src = require('../../resources/icons/f.png').default;
+        break;
+      case 6:
+        icon_src = require('../../resources/icons/g.png').default;
+        break;
+      case 7:
+        icon_src = require('../../resources/icons/h.png').default;
+        break;
+      default:
+        break;
+    }
+    return icon_src;
+  };
+
   return (
     <div className={'chat_record_page '.concat(props.className)}>
       <div className='word_list'>
         {chatRecords_1.map(it => {
           return (
-            <div className='word_item'>
-              {/* <div>{it.char}</div> */}
-              <img src={require('../../resources/icons/a.png').default} />
-              <div className='words'>{it.words}</div>
+            <div className={'word_item '.concat(it.char == words_obj.words_1.me_id ? 'chat_box_right' : '').concat()}>
+              <img src={get_icon_src(it.char)} className='icon' />
+              {it.words[0] === '\t' || (
+                <img src={require('../../resources/icons/horn.png').default} className='icon_horn' />
+              )}
+              <div className={'words '.concat(it.words[0] === '\t' ? 'border_none' : '')}>{it.words}</div>
             </div>
           );
         })}
